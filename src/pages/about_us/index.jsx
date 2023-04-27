448e61f774a5d560159334551eafdc7496552bb2
CAPVOK <capvok@yandex.ru>
2023-04-27 20:29:29 +0300

    aboutus

--- src/pages/about_us/index.jsx
+++ src/pages/about_us/index.jsx
@@ -5,8 +5,101 @@ export const AboutUs = () => {
     return (
         <>
             <Header/>
-            <div className='container mx-auto text-white mb-5 text-xl font-[600] bg-white/30 backdrop-blur-sm rounded-md h-screen p-5 mt-16 sm:mt-5'>
-                About Us
+            <div class="w-full h-screen">
+                <div class="flex flex-col lg:grid lg:gap-4 2xl:gap-6 lg:grid-cols-12 2xl:row-span-2 2xl:pb-8 ml-2 pt-4 px-6">
+                    {/* Краснов */}
+                    <div class="bg-indigo-600 lg:order-1 lg:row-span-1 lg:col-span-7 rounded-lg shadow-xl mb-5 lg:mb-0">
+                        <div class="mx-6 my-8 2xl:mx-10 flex flex-row items-center">
+                            <div className="bg-cover w-20 h-20 rounded-full border-2 ml-3" style={{backgroundImage: 'url(/1.jpg)'}}></div>
+                            <div className="flex flex-col ml-4">
+                                <h1 class="text-white text-lg ">Краснов Владимир</h1>
+                                <h2 class="text-white text-opacity-50 text-base ">Backend</h2>
+                            </div>
+                        </div>
+                        <div class="-mt-6 relative">
+                            <p class="text-white text-xl 2xl:text-2xl font-bold px-7 lg:px-9 2xl:pt-6 2xl:mx-2">
+                                I received a job offer mid-course, and the subjects I learned were current, if not more so, 
+                                in the company I joined. I honestly feel I got every penny’s worth.</p>
+                            <br />
+                            <p class="text-white text-opacity-50 font-medium md:text-sm 2xl:text-xl px-7 lg:px-9 mb-3 2xl:pb-8 2xl:mx-2">
+                                “ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a 
+                                transition and have heard some people who had an amazing experience here. I signed up 
+                                for the free intro course and found it incredibly fun! I enrolled shortly thereafter. 
+                                The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, 
+                                I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”</p>
+                        </div>
+                    </div>
+
+                    {/* Каримов */}
+                    <div class="bg-indigo-600 lg:order-1 lg:row-span-1 lg:col-span-5 rounded-lg shadow-xl mb-5 lg:mb-0">
+                        <div class="mx-6 my-8 2xl:mx-10 flex flex-row items-center">
+                            <div className="bg-cover w-20 h-20 rounded-full border-2 ml-3" style={{backgroundImage: 'url(/4.jpg)'}}></div>
+                            <div className="flex flex-col ml-4">
+                                <h1 class="text-white text-lg ">Каримов Амонулло</h1>
+                                <h2 class="text-white text-opacity-50 text-base ">Frontend</h2>
+                            </div>
+                        </div>
+                        <div class="-mt-6 relative">
+                            <p class="text-white text-xl 2xl:text-2xl font-bold px-7 lg:px-9 2xl:pt-6 2xl:mx-2">
+                                I received a job offer mid-course, and the subjects I learned were current, if not more so, 
+                                in the company I joined. I honestly feel I got every penny’s worth.</p>
+                            <br />
+                            <p class="text-white text-opacity-50 font-medium md:text-sm 2xl:text-xl px-7 lg:px-9 mb-3 2xl:pb-8 2xl:mx-2">
+                                “ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a 
+                                transition and have heard some people who had an amazing experience here. I signed up 
+                                for the free intro course and found it incredibly fun! I enrolled shortly thereafter. 
+                                The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, 
+                                I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”</p>
+                        </div>
+                    </div>
+
+                    {/* Кабанец */}
+                    <div class="bg-indigo-600 lg:order-1 lg:row-span-1 lg:col-span-5 rounded-lg shadow-xl mb-5 lg:mb-0">
+                        <div class="mx-6 my-8 2xl:mx-10 flex flex-row items-center">
+                            <div className="bg-cover w-20 h-20 rounded-full border-2 ml-3" style={{backgroundImage: 'url(/3.jpg)'}}></div>
+                            <div className="flex flex-col ml-4">
+                                <h1 class="text-white text-lg ">Кабанец Владимир</h1>
+                                <h2 class="text-white text-opacity-50 text-base ">Frontend</h2>
+                            </div>
+                        </div>
+                        <div class="-mt-6 relative">
+                            <p class="text-white text-xl 2xl:text-2xl font-bold px-7 lg:px-9 2xl:pt-6 2xl:mx-2">
+                                I received a job offer mid-course, and the subjects I learned were current, if not more so, 
+                                in the company I joined. I honestly feel I got every penny’s worth.</p>
+                            <br />
+                            <p class="text-white text-opacity-50 font-medium md:text-sm 2xl:text-xl px-7 lg:px-9 mb-3 2xl:pb-8 2xl:mx-2">
+                                “ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a 
+                                transition and have heard some people who had an amazing experience here. I signed up 
+                                for the free intro course and found it incredibly fun! I enrolled shortly thereafter. 
+                                The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, 
+                                I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”</p>
+                        </div>
+                    </div>
+
+                    {/* Толкачов */}
+                    <div class="bg-indigo-600 lg:order-1 lg:row-span-1 lg:col-span-7 rounded-lg shadow-xl mb-5 lg:mb-0">
+                        <div class="mx-6 my-8 2xl:mx-10 flex flex-row items-center">
+                            <div className="bg-cover w-20 h-20 rounded-full border-2 ml-3" style={{backgroundImage: 'url(/2.jpg)'}}></div>
+                            <div className="flex flex-col ml-4">
+                                <h1 class="text-white text-lg ">Толкачев Родион</h1>
+                                <h2 class="text-white text-opacity-50 text-base ">Frontend</h2>
+                            </div>
+                        </div>
+                        <div class="-mt-6 relative">
+                            <p class="text-white text-xl 2xl:text-2xl font-bold px-7 lg:px-9 2xl:pt-6 2xl:mx-2">
+                                I received a job offer mid-course, and the subjects I learned were current, if not more so, 
+                                in the company I joined. I honestly feel I got every penny’s worth.</p>
+                            <br />
+                            <p class="text-white text-opacity-50 font-medium md:text-sm 2xl:text-xl px-7 lg:px-9 mb-3 2xl:pb-8 2xl:mx-2">
+                                “ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a 
+                                transition and have heard some people who had an amazing experience here. I signed up 
+                                for the free intro course and found it incredibly fun! I enrolled shortly thereafter. 
+                                The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, 
+                                I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”</p>
+                        </div>
+                    </div>
+                    {/* <!-- Ending of the component about Patrick Abrams -->*/}
+                </div>
             </div>
         </>
     );
