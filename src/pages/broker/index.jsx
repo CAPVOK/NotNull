3b0b696b7db30919aa91f0b165bc05f389f18aea
CAPVOK <capvok@yandex.ru>
2023-04-29 16:34:38 +0300

    broker

--- src/pages/broker/index.jsx
+++ src/pages/broker/index.jsx
@@ -16,35 +16,34 @@ export const Broker = () => {
     console.log(request); */
 
     return (
-        <>
-            <div className=" w-11/12 mx-auto bg-gradient-to-br from-white/20 to-white/10 backdrop-blur-md rounded-lg p-5 mt-5">
-                <div className="mb-5 text-white font-normal text-xl flex justify-between items-center ml-[25px]">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className=" fixed left-[20px] w-6 h-6 cursor-pointer"
-                        onClick={() => navigate(-1)}
-                    >
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-                    </svg>
-                    <div className="mx-5">{brokerName}</div>
-                    {/* <div className="mx-5">Status: Undefainded</div> */}
-                </div>
-                <div className="flex flex-col min-h-screen p-3 text-xl text-white">
-                {
-                    
-                    request && 
-                    request.request.supportedCommands.map((command, key) => {
-                        return (
-                            <Accordion key={key} id={command.alias} sender={brokerName}/>
-                        )
-                    })
-                }
-                </div>
+        <div className="min-h-[95vh] mt-5 w-11/12 p-0 pt-5 sm:p-5 mx-auto bg-gradient-to-br from-white/30 to-white/20 backdrop-blur-md rounded-xl ">
+            <div onClick={() => navigate(-1)} 
+            className="flex flex-row w-min justify-start space-x-5 items-center ml-[25px] text-white font-normal text-xl hover:text-amber-400 hover:font-medium cursor-pointer">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className=" w-6 h-6 "
+                >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                </svg>
+                <div className="text-lg sm:text-xl ">{brokerName}</div>
+                {/* <div className="mx-5">Status: Undefainded</div> */}
             </div>
-        </>
+            <div className="flex flex-col p-3 text-xl text-white">
+            {
+                request && 
+                request.request.supportedCommands.map((command, key) => {
+                    return (
+                        <div className="">
+                            <Accordion key={key} id={command.alias} sender={brokerName}/>
+                        </div>
+                    )
+                })
+            }
+            </div>
+        </div>
     )
 }
