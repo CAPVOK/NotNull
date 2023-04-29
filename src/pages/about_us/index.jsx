de78ac41ffd0fb811a3a95f1243d811ffba2b78b
CAPVOK <capvok@yandex.ru>
2023-04-30 00:07:21 +0300

    about

--- src/pages/about_us/index.jsx
+++ src/pages/about_us/index.jsx
@@ -1,14 +1,21 @@
 import Header from "../modules/header";
+import { useEffect, useState } from "react";
 
 export const AboutUs = () => {
+
+    const [isAnimated, setAnimated] = useState(false);
+
+    useEffect(()=>{
+        setAnimated(true);
+    })
 
     return (
         <>
             <Header/>
             <div className="w-full h-full mt-12 md:mt-0">
-                <div className="flex flex-col lg:grid lg:gap-6 2xl:gap-8 lg:grid-cols-12 2xl:pb-8 ml-2 pt-4 px-6">
+                <div className={`flex flex-col lg:grid lg:gap-6 2xl:gap-8 lg:grid-cols-12 2xl:pb-8 ml-2 pt-4 px-6`}>
                     {/* Краснов */}
-                    <div className="bg-amber-500 lg:order-1 lg:row-span-1 lg:col-span-7 rounded-lg shadow-xl mb-5 lg:mb-0">
+                    <div className={`beforeAnimated1 ${isAnimated ? 'afterAnimated' : ''} bg-amber-500 lg:order-1 lg:row-span-1 lg:col-span-7 rounded-lg shadow-xl mb-5 lg:mb-0`}>
                         <div className="mx-6 my-8 2xl:mx-10 flex flex-row items-center">
                             <div className="bg-cover w-20 h-20 lg:w-32 lg:h-32 rounded-full border-2 ml-3" style={{backgroundImage: 'url(/vova_krasniy.jpg)'}}></div>
                             <div className="flex flex-col ml-4">
@@ -30,7 +37,7 @@ export const AboutUs = () => {
                     </div>
 
                     {/* Каримов */}
-                    <div className="bg-indigo-600 lg:order-1 lg:row-span-1 lg:col-span-5 rounded-lg shadow-xl mb-5 lg:mb-0">
+                    <div className={`beforeAnimated2 ${isAnimated ? 'afterAnimated' : ''} bg-indigo-600 lg:order-1 lg:row-span-1 lg:col-span-5 rounded-lg shadow-xl mb-5 lg:mb-0`}>
                         <div className="mx-6 my-8 2xl:mx-10 flex flex-row items-center">
                             <div className="bg-cover w-20 h-20 lg:w-32 lg:h-32 rounded-full border-2 ml-3" style={{backgroundImage: 'url(/amon.jpg)'}}></div>
                             <div className="flex flex-col ml-4">
@@ -50,7 +57,7 @@ export const AboutUs = () => {
                     </div>
 
                     {/* Кабанец */}
-                    <div className="bg-indigo-600 lg:order-3 lg:row-span-1 lg:col-span-5 rounded-lg shadow-xl mb-5 lg:mb-0">
+                    <div className={`beforeAnimated1 ${isAnimated ? 'afterAnimated' : ''} bg-indigo-600 lg:order-3 lg:row-span-1 lg:col-span-5 rounded-lg shadow-xl mb-5 lg:mb-0`}>
                         <div className="mx-6 my-8 2xl:mx-10 flex flex-row items-center">
                             <div className="bg-cover w-20 h-20 lg:w-32 lg:h-32 rounded-full border-2 ml-3" style={{backgroundImage: 'url(/kaban.jpg)'}}></div>
                             <div className="flex flex-col ml-4">
@@ -69,7 +76,7 @@ export const AboutUs = () => {
                     </div>
 
                     {/* Толкачeв */}
-                    <div className="bg-amber-500 lg:order-4 lg:row-span-1 lg:col-span-7 rounded-lg shadow-xl mb-5 lg:mb-0">
+                    <div className={`beforeAnimated2 ${isAnimated ? 'afterAnimated' : ''} bg-amber-500 lg:order-4 lg:row-span-1 lg:col-span-7 rounded-lg shadow-xl mb-5 lg:mb-0`}>
                         <div className="mx-6 my-8 2xl:mx-10 flex flex-row items-center">
                             <div className="bg-cover w-20 h-20 lg:w-32 lg:h-32 rounded-full border-2 ml-3" style={{ backgroundImage: 'url(/rodya.jpg)'}}></div>
                             <div className="flex flex-col ml-4">
