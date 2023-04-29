19278c23b2ec0394e3949ae03b486f4ecd6966d7
CAPVOK <capvok@yandex.ru>
2023-04-29 15:07:25 +0300

    sidebar

--- src/pages/modules/components/sidebar/index.jsx
+++ src/pages/modules/components/sidebar/index.jsx
@@ -9,17 +9,19 @@ export const SideBar = (data) => {
     const requests = useSelector((state)=>state.request.requests);
 
     return (
-        <div className="h-[full] top-0 left-0 flex flex-col text-white bg-white/30 backdrop-blur-sm place-content-start text-center text-2xl " >
-            <p className="my-2 mx-1 mt-5">Сервисы БИ</p>
-            <hr className="border mx-3 "/>
-            {
-                (requests?.length > 0) && requests.map((item, index) => (
-                    <div key={index}
-                    className=" cursor-pointer hover:bg-white/20 mx-3 my-2 py-2 px-3 rounded-md"
-                    onClick={()=>clickTable(item.header.sender)}    
-                    >{item.header.sender}</div>
-                ) )
-            }
-        </div>
+
+            <div className="mt-16 sm:mt-4 m-4 md:m-5 lg:m-6 w-6/12 md:w-5/12 lg:w-4/12 flex flex-col text-white bg-gradient-to-br from-white/30 rounded-xl to-white/20 backdrop-blur-md shadow-xl place-content-start text-center text-2xl " >
+                <p className="mx-1 mt-6 mb-4 ">Сервисы БИ</p>
+                <b className="border-[1px] w-11/12 mx-auto border-gray-300"/>
+                {
+                    (requests?.length > 0) && requests.map((item, index) => (
+                        <div key={index}
+                        className=" cursor-pointer hover:bg-white/20 mx-3 my-2 py-2 px-3 rounded-md font-normal"
+                        onClick={()=>clickTable(item.header.sender)}    
+                        >{item.header.sender}</div>
+                    ) )
+                }
+            </div>
+
     );
 }
