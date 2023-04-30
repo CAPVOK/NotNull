12de7e9d50d22b97da8d2e29710812f7d297c0c8
VEN0M416 <amonullo.k.02@mail.ru>
2023-04-30 06:15:44 +0300

    bit lazy

--- src/pages/modules/components/bitcoin/index.jsx
+++ src/pages/modules/components/bitcoin/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import Spline from '@splinetool/react-spline';
 
-export const Bitcoin = () => {
-    return (
-        <Spline 
-          scene="https://prod.spline.design/pb3tqkTKTxaEVzXU/scene.splinecode" 
-          width={400}
-          height={400}
-          position={{ x: 0.5, y: 0.5 }}
-        />
-      );
+export default function Bitcoin() {
+  return (
+    <Spline
+      scene="https://prod.spline.design/pb3tqkTKTxaEVzXU/scene.splinecode"
+    />
+  );
 }
--- src/pages/test1/index.jsx
+++ src/pages/test1/index.jsx
@@ -1,3 +1,14 @@
+import React, {Suspense} from "react";
+import { lazy } from "react";
+const Bitcoin = lazy(()=>import("../modules/components/bitcoin"));
 export const Test1 = () => {
-    
+    return(
+
+        <div>
+            <Suspense fallback={<div className="text-white text-4xl">Loading....</div>}>
+                <Bitcoin/>
+            </Suspense>
+        </div>
+
+    );
 };
