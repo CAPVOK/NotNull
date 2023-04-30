1396dd84e56e49874329e77116b4e87d3737ae86
CAPVOK <capvok@yandex.ru>
2023-04-30 05:04:47 +0300

    new about

--- src/pages/about_us/index.jsx
+++ src/pages/about_us/index.jsx
@@ -39,7 +39,7 @@ export const AboutUs = () => {
                     {/* Каримов */}
                     <div className={`beforeAnimated2 ${isAnimated ? 'afterAnimated' : ''} bg-gradient-to-br from-white/30 to-white/10 backdrop-blur-md border-[1px] border-gray-500 lg:order-1 lg:row-span-1 lg:col-span-5 rounded-lg shadow-xl mb-5 lg:mb-0`}>
                         <div className="mx-6 my-8 2xl:mx-10 flex flex-row items-center">
-                            <div className="bg-cover w-20 h-20 lg:w-32 lg:h-32 rounded-full border-2 ml-3" style={{backgroundImage: 'url(/amon.jpg)'}}></div>
+                            <div className="bg-cover bg-center w-20 h-20 lg:w-32 lg:h-32 rounded-full border-2 ml-3" style={{backgroundImage: 'url(/amon.jpg)'}}></div>
                             <div className="flex flex-col ml-4">
                                 <h1 className="text-white text-xl lg:text-2xl ">Каримов Амонулло</h1>
                                 <h2 className="text-white text-opacity-50 text-base ">Frontend</h2>
@@ -78,7 +78,7 @@ export const AboutUs = () => {
                     {/* Толкачeв */}
                     <div className={`beforeAnimated2 ${isAnimated ? 'afterAnimated' : ''} bg-amber-500 text-black lg:order-4 lg:row-span-1 lg:col-span-7 rounded-lg shadow-xl mb-5 lg:mb-0`}>
                         <div className="mx-6 my-8 2xl:mx-10 flex flex-row items-center">
-                            <div className="bg-cover w-20 h-20 lg:w-32 lg:h-32 rounded-full border-2 ml-3" style={{ backgroundImage: 'url(/rodya.jpg)'}}></div>
+                            <div className="bg-cover bg-center w-20 h-20 lg:w-32 lg:h-32 rounded-full border-2 ml-3" style={{ backgroundImage: 'url(/rodion.jpg)'}}></div>
                             <div className="flex flex-col ml-4">
                                 <h1 className="text-xl lg:text-2xl ">Толкачeв Родион</h1>
                                 <h2 className="text-opacity-50 text-base ">Frontend</h2>
