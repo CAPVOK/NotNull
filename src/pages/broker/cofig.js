81948ecc5072ef078ce2a14bbe7b4ce22d6892f7
VEN0M416 <amonullo.k.02@mail.ru>
2023-04-30 10:30:20 +0300

    gyy

---
+++ src/pages/broker/cofig.js
@@ -0,0 +1,11 @@
+import { useSelector } from "react-redux";
+
+export function getCommand (data){
+    return(data.response.command);
+}
+export function getCommand (data){
+    return(data.response.answerType);
+}
+export function getCommand (data){
+    return();
+}
