03661cf6153ab2f2bb459c0cb387debfb90a122e
VEN0M416 <amonullo.k.02@mail.ru>
2023-05-01 20:07:57 +0300

    бывает

--- src/pages/broker/index.jsx
+++ src/pages/broker/index.jsx
@@ -2,7 +2,6 @@ import { useParams } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 import { Accordion } from "../modules/components/accordion";
 import { useSelector } from "react-redux";
-import { async } from "q";
 export const Broker = () => {
     const brokerName = useParams().broker;
     const navigate = useNavigate();
@@ -14,11 +13,11 @@ export const Broker = () => {
     })
     const response = useSelector((state) => state.request.response);
     console.log(response);
+    const DataRows = response?.response.status.advStatus.data.rows;
+    console.log(DataRows);
 
-    const bord = "border-2 border-gray-300"
-    /* bg-gradient-to-br from-white/30 to-white/20 backdrop-blur-md */
     return (
-        <div className="min-h-[95vh] mt-5 w-11/12 p-0 pt-5 sm:p-5 mx-auto bg-transparent rounded-xl ">
+        <div className="min-h-[95vh] mt-5 w-11/12 p-0 pt-5 sm:p-5 mx-auto rounded-xl ">
             {/* header */}
             <div onClick={() => navigate(-1)}
                 className="flex p-2 px-3 flex-row w-min justify-start space-x-5 items-center rounded-lg text-white font-normal text-xl cursor-pointer hover:bg-white/20">
@@ -36,50 +35,43 @@ export const Broker = () => {
                 {/* <div className="mx-5">Status: Undefainded</div> */}
             </div>
             {/* response */}
-            <div className="w-full mt-5">
-                {/* <table className={`text-white w-full ${bord}`}>
-                    <thead>
-                        <tr className={`text-white ${bord}`}>
-                            <th className={`text-white ${bord}`}>command</th>
-                            <th className={`text-white ${bord}`}>answerType</th>
-                            {response.errorText && <th className={`text-white ${bord}`}>errorText</th>}
-                            {response.status && <th className={`text-white ${bord}`}>
-                                <div className="border-b-2 border-gray-300">{response.status.advStatus.caption}</div>
-                                <div className="grid grid-flow-col">
-                                    {response.status.advStatus.fields.map((item, index) =>
-                                        <div key={index} className="border-r-2 border-gray-300">{item.alias}</div>
-                                    )}
-                                </div>
-                            </th>}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <th className={`text-white ${bord}`}>
-                            {response.command}
-                        </th>
-                        <th className={`text-white ${bord}`}>
-                            {response.answerType}
-                        </th>
-                        {response.errorText && <th className={`text-white ${bord}`}>
-                            {response.errorText}
-                        </th>}
-                        {response.status && <th className={`text-white ${bord}`}>
-                            <div className="grid grid-flow-col">
-                                {response.status.advStatus.data.rows.map((item, index) =>
-                                    <div key={index} className="border-r-2 border-gray-300">
-                                        <div className="flex flex-col">
-                                            {item.values.map((value, index) =>
-                                                <div>{value.value}</div>
-                                            )}</div>
-                                    </div>
-                                )}
-                            </div>
-                        </th>}
-                    </tbody>
-                </table> */}
+            <div className="w-full mt-5 text-white">
+                <div className="bg-white/20 backdrop-blur-md p-5 w-max rounded-lg">
+                    {DataRows ? <table>
+                        <thead>
+                            <tr>
+                                <th className="border-[1px]">Id</th>
+                                {
+                                    DataRows[0].values.map((data, key) => (
+                                        <th key={key} className="border-[1px] px-3">{data.alias}</th>
+                                    ))
+                                }
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                DataRows.map((data, key) => (
+                                    <tr key={key} className="border-[1px]">
+                                        {
+                                            <>
+                                                <td className="border-[1px] px-3">{data.rowIdent}</td>
+                                                {
+                                                    data.values.map((item, key) => (
+                                                        <td key={key} className="border-[1px] px-3">{item.value.value}</td>
+                                                    ))
+                                                }
+
+                                            </>
+                                        }
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table> : <p>Loading data...</p>}
+                </div>
             </div>
             {/* list */}
-            <div className="flex flex-col p-3 text-xl text-white">
+            <div className="flex flex-col text-xl text-white">
                 {
                     request &&
                     request.request.supportedCommands.map((command, key) => {
